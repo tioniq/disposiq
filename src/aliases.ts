7bef6be38fc2ad56c9a0bc3865da2602ffc31acf
Timoniann <timoniann@gmail.com>
2024-09-22 11:46:36 +0300

    chore: added alias for AsyncDisposiq

--- src/aliases.ts
+++ src/aliases.ts
@@ -4,13 +4,14 @@ import {BoolDisposable} from "./bool";
 import {disposeAll} from "./dispose-batch";
 import {disposableFromEvent, disposableFromEventOnce} from "./event";
 import {createDisposable, createDisposableCompat, createDisposiq} from "./create";
-import {Disposiq} from "./disposiq";
+import {AsyncDisposiq, Disposiq} from "./disposiq";
 
 export {
   BoolDisposable as BooleanDisposable,
   DisposableContainer as SerialDisposable,
   DisposableStore as CompositeDisposable,
   Disposiq as BaseDisposable,
+  AsyncDisposiq as BaseAsyncDisposable,
   disposeAll as disposeAllSafe,
   disposableFromEvent as on,
   disposableFromEventOnce as once,
