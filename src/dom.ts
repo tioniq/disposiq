42ebaf7730782c15986e57c65e5a9b08ce32643d
Timoniann <timoniann@gmail.com>
2024-11-10 03:04:11 +0200

    chore: make type checker happy for addEventListener listener param

--- src/dom.ts
+++ src/dom.ts
@@ -1,7 +1,7 @@
 import { DisposableAction } from "./action";
 import { Disposiq } from "./disposiq";
 
-type EventListener = ((this: EventTarget, ev: Event) => any) | {
+type EventListener<T extends Event = Event> = ((this: EventTarget, ev: T) => any) | {
   handleEvent(evt: Event): void
 }
 
@@ -16,12 +16,12 @@ interface AddEventListenerOptions extends EventListenerOptions {
 }
 
 interface EventTarget {
-  addEventListener(type: string, listener: EventListener, options?: boolean | AddEventListenerOptions): void
+  addEventListener<E extends Event>(type: string, listener: EventListener<E>, options?: boolean | AddEventListenerOptions): void
 
-  removeEventListener(type: string, listener: EventListener, options?: boolean | EventListenerOptions): void
+  removeEventListener<E extends Event>(type: string, listener: EventListener<E>, options?: boolean | EventListenerOptions): void
 }
 
-export function addEventListener(target: EventTarget, type: string, listener: EventListener, options?: boolean | AddEventListenerOptions): Disposiq {
+export function addEventListener<E extends Event>(target: EventTarget, type: string, listener: EventListener<E>, options?: boolean | AddEventListenerOptions): Disposiq {
   target.addEventListener(type, listener, options)
   return new DisposableAction(() => target.removeEventListener(type, listener, options))
 }
